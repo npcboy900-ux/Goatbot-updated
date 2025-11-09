f68fb2e258e9cc4cc36a0b62d0bfe98b9eb3a3db
npcboy900-ux <npcboy900@gmail.com>
2025-11-09 19:39:51 +0700

    Update admin.js

--- scripts/cmds/admin.js
+++ scripts/cmds/admin.js
@@ -5,33 +5,35 @@ module.exports = {
 	config: {
 		name: "admin",
 		version: "1.6",
-		author: "NTKhang",
+		author: "NEVELOP SIZE56",
 		countDown: 5,
 		role: 2,
 		description: {
-			vi: "Thêm, xóa, sửa quyền admin",
-			en: "Add, remove, edit admin role"
+			th: "เพิ่ม ลบ หรือแก้ไขสิทธิ์แอดมินของบอท",
+			en: "Add, remove, or edit admin role of the bot"
 		},
 		category: "box chat",
 		guide: {
-			vi: '   {pn} [add | -a] <uid | @tag>: Thêm quyền admin cho người dùng'
-				+ '\n	  {pn} [remove | -r] <uid | @tag>: Xóa quyền admin của người dùng'
-				+ '\n	  {pn} [list | -l]: Liệt kê danh sách admin',
-			en: '   {pn} [add | -a] <uid | @tag>: Add admin role for user'
-				+ '\n	  {pn} [remove | -r] <uid | @tag>: Remove admin role of user'
-				+ '\n	  {pn} [list | -l]: List all admins'
+			th:
+				'   {pn} [add | -a] <uid | @แท็ก>: เพิ่มสิทธิ์แอดมินให้ผู้ใช้' +
+				'\n   {pn} [remove | -r] <uid | @แท็ก>: ลบสิทธิ์แอดมินของผู้ใช้' +
+				'\n   {pn} [list | -l]: แสดงรายชื่อผู้ดูแลทั้งหมด',
+			en:
+				'   {pn} [add | -a] <uid | @tag>: Add admin role for user' +
+				'\n   {pn} [remove | -r] <uid | @tag>: Remove admin role of user' +
+				'\n   {pn} [list | -l]: List all admins'
 		}
 	},
 
 	langs: {
-		vi: {
-			added: "✅ | Đã thêm quyền admin cho %1 người dùng:\n%2",
-			alreadyAdmin: "\n⚠️ | %1 người dùng đã có quyền admin từ trước rồi:\n%2",
-			missingIdAdd: "⚠️ | Vui lòng nhập ID hoặc tag người dùng muốn thêm quyền admin",
-			removed: "✅ | Đã xóa quyền admin của %1 người dùng:\n%2",
-			notAdmin: "⚠️ | %1 người dùng không có quyền admin:\n%2",
-			missingIdRemove: "⚠️ | Vui lòng nhập ID hoặc tag người dùng muốn xóa quyền admin",
-			listAdmin: "👑 | Danh sách admin:\n%1"
+		th: {
+			added: "✅ | เพิ่มสิทธิ์แอดมินให้กับ %1 ผู้ใช้แล้ว:\n%2",
+			alreadyAdmin: "\n⚠️ | %1 ผู้ใช้มีสิทธิ์แอดมินอยู่แล้ว:\n%2",
+			missingIdAdd: "⚠️ | โปรดกรอก ID หรือแท็กผู้ใช้ที่ต้องการเพิ่มสิทธิ์แอดมิน",
+			removed: "✅ | ลบสิทธิ์แอดมินของ %1 ผู้ใช้แล้ว:\n%2",
+			notAdmin: "⚠️ | %1 ผู้ใช้นี้ไม่มีสิทธิ์แอดมิน:\n%2",
+			missingIdRemove: "⚠️ | โปรดกรอก ID หรือแท็กผู้ใช้ที่ต้องการลบสิทธิ์แอดมิน",
+			listAdmin: "👑 | รายชื่อผู้ดูแลระบบทั้งหมด:\n%1"
 		},
 		en: {
 			added: "✅ | Added admin role for %1 users:\n%2",
@@ -56,8 +58,10 @@ module.exports = {
 						uids.push(event.messageReply.senderID);
 					else
 						uids = args.filter(arg => !isNaN(arg));
+
 					const notAdminIds = [];
 					const adminIds = [];
+
 					for (const uid of uids) {
 						if (config.adminBot.includes(uid))
 							adminIds.push(uid);
@@ -66,16 +70,19 @@ module.exports = {
 					}
 
 					config.adminBot.push(...notAdminIds);
-					const getNames = await Promise.all(uids.map(uid => usersData.getName(uid).then(name => ({ uid, name }))));
+					const getNames = await Promise.all(uids.map(uid =>
+						usersData.getName(uid).then(name => ({ uid, name }))
+					));
+
 					writeFileSync(global.client.dirConfig, JSON.stringify(config, null, 2));
+
 					return message.reply(
-						(notAdminIds.length > 0 ? getLang("added", notAdminIds.length, getNames.map(({ uid, name }) => `• ${name} (${uid})`).join("\n")) : "")
-						+ (adminIds.length > 0 ? getLang("alreadyAdmin", adminIds.length, adminIds.map(uid => `• ${uid}`).join("\n")) : "")
+						(notAdminIds.length > 0 ? getLang("added", notAdminIds.length, getNames.map(({ uid, name }) => `• ${name} (${uid})`).join("\n")) : "") +
+						(adminIds.length > 0 ? getLang("alreadyAdmin", adminIds.length, adminIds.map(uid => `• ${uid}`).join("\n")) : "")
 					);
-				}
-				else
-					return message.reply(getLang("missingIdAdd"));
+				} else return message.reply(getLang("missingIdAdd"));
 			}
+
 			case "remove":
 			case "-r": {
 				if (args[1]) {
@@ -84,31 +91,41 @@ module.exports = {
 						uids = Object.keys(event.mentions)[0];
 					else
 						uids = args.filter(arg => !isNaN(arg));
+
 					const notAdminIds = [];
 					const adminIds = [];
+
 					for (const uid of uids) {
 						if (config.adminBot.includes(uid))
 							adminIds.push(uid);
 						else
 							notAdminIds.push(uid);
 					}
+
 					for (const uid of adminIds)
 						config.adminBot.splice(config.adminBot.indexOf(uid), 1);
-					const getNames = await Promise.all(adminIds.map(uid => usersData.getName(uid).then(name => ({ uid, name }))));
+
+					const getNames = await Promise.all(adminIds.map(uid =>
+						usersData.getName(uid).then(name => ({ uid, name }))
+					));
+
 					writeFileSync(global.client.dirConfig, JSON.stringify(config, null, 2));
+
 					return message.reply(
-						(adminIds.length > 0 ? getLang("removed", adminIds.length, getNames.map(({ uid, name }) => `• ${name} (${uid})`).join("\n")) : "")
-						+ (notAdminIds.length > 0 ? getLang("notAdmin", notAdminIds.length, notAdminIds.map(uid => `• ${uid}`).join("\n")) : "")
+						(adminIds.length > 0 ? getLang("removed", adminIds.length, getNames.map(({ uid, name }) => `• ${name} (${uid})`).join("\n")) : "") +
+						(notAdminIds.length > 0 ? getLang("notAdmin", notAdminIds.length, notAdminIds.map(uid => `• ${uid}`).join("\n")) : "")
 					);
-				}
-				else
-					return message.reply(getLang("missingIdRemove"));
+				} else return message.reply(getLang("missingIdRemove"));
 			}
+
 			case "list":
 			case "-l": {
-				const getNames = await Promise.all(config.adminBot.map(uid => usersData.getName(uid).then(name => ({ uid, name }))));
+				const getNames = await Promise.all(config.adminBot.map(uid =>
+					usersData.getName(uid).then(name => ({ uid, name }))
+				));
 				return message.reply(getLang("listAdmin", getNames.map(({ uid, name }) => `• ${name} (${uid})`).join("\n")));
 			}
+
 			default:
 				return message.SyntaxError();
 		}
