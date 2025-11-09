be3d2c7d1a712043fed17a90fb8ad26e6f6398fb
npcboy900-ux <npcboy900@gmail.com>
2025-11-10 06:47:02 +0700

    sp.js

--- scripts/cmds/busy.js
+++ scripts/cmds/busy.js
@@ -4,7 +4,7 @@ module.exports = {
 		version: "1.0",
 		author: "NTKhang",
 		countDown: 5,
-		role: 2,
+		role: 0,
 		description: {
 			en: "Change the group name multiple times with numbering"
 		},
